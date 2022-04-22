a3939bf8e6c5372aa7768a1b5763c07952914b26
Christophe Denutte <christophe.denutte@live.be>
2022-04-22 15:29:22 +0200

    Create Project

---
+++ Nutz.Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Nutz.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Nutz.Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nutz.Utility
+{
+    // 000 - Create Project
+    public static class SD
+    {
+    }
+}
